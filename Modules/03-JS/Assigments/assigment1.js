8eafc95476fee2004fe596800346f321d8ea87bd
Henry Eng <100420050+Henryeng316@users.noreply.github.com>
2022-05-03 13:15:19 +0800

    Add files via upload
    JS Assignment 1

---
+++ Modules/03-JS/Assigments/assigment1.js
@@ -0,0 +1,15 @@
+//JS Assigment 1
+//Calculate the sum of all numbers
+/*Given an array of integers, find the sum of all integwrs.You can be assure that
+the array will only contain integers(both positive and negative).
+*/
+
+function sum(numbersArray) {   
+    let num = 0;
+    for ( let i = 0; i < numbersArray.length; i++) {
+       num += numbersArray[i];
+    }
+    return num
+  }
+  console.log(sum([1, 2, 3, 4]));
+  console.log(sum([-3, 5, 19, -6]));
